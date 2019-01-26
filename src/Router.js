4044ca42a20a043b829db2a29c0cb8966f393f12
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-01-25 19:20:51 -0500

    add assignment redirect route

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import App from "./App";
 import Similarity from "./Similarity";
 import Coverage from "./Coverage";
@@ -12,6 +12,11 @@ class MyRouter extends Component {
                   <Route path="/similarity/:set" component={Similarity}/>
                   <Route path="/similarity" component={Similarity}/>
                   <Route path="/coverage" component={Coverage}/>
+                  <Route path="/assignments" render={() => {
+                      window.location = "https://car-cs.herokuapp.com/assignments";
+                      return null;
+                    }
+                  }/>
                   <Route path="/" component={App}/>
               </Switch>
           </Router>
